e507ecae4472720b8928834e9cdf98d987de9d1f
Vishal <78644292+pvrvishal@users.noreply.github.com>
2021-11-16 16:35:48 +0530

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get("DEBUG", False)
 
 ALLOWED_HOSTS = ['vishalparmar.herokuapp.com']
 
