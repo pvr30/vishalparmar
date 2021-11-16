dbbfde0158bd549181bffa167629c09f4c652760
Vishal <78644292+pvrvishal@users.noreply.github.com>
2021-11-16 16:44:31 +0530

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.environ.get("DEBUG", False)
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['vishalparmar.herokuapp.com']
 
