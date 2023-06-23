931fb26b41e7d99a816134c9fe2043c48fafe338
vishal <vishalparmar6958@gmail.com>
2023-06-23 18:34:24 +0530

    deploy on vercel

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -20,13 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'maskldsiohdw2323l32jldlksndknsds'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.environ.get("DEBUG", False)
 DEBUG = False
 
-ALLOWED_HOSTS = ['vishalparmar.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 # ALLOWED_HOSTS = ['*']
 
 
